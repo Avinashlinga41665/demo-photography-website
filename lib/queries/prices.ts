d560139d58bdaa26047e74c76b163b38a5e58313
AvinashLinga <avinashlinga41665@gmail.com>
2025-12-06 20:38:17 +0530

    Price Query Fix

--- lib/queries/prices.ts
+++ lib/queries/prices.ts
@@ -1,12 +1,20 @@
 export const GET_PRICES = `
   query GetPrices {
-  pricePackages {
-    id
-    title
-    price
-    editphotonumber
-    photo1 { url }
-    photo2 { url }
-    photo3 { url }
+    pricePackages {
+      id
+      title
+      price
+      editphotonumber
+
+      photo1 {
+        url
+      }
+      photo2 {
+        url
+      }
+      photo3 {
+        url
+      }
+    }
   }
-};
+`;
